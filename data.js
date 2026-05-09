88333de6505d3e2509f944d1e5660e08acdffa6c
rensoo2803-crypto <rensoo2803@gmail.com>
2026-05-09 22:59:40 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+const allvoca = [
+{ dan: "あ ", word: "愛妻家", reading: "あいさいか", meaning: "애처가", sentence: "吉田さんは愛妻家です。", translation: "よしだ씨는애처가입니다. " },
+{ dan: "あ ", word: "挨拶", reading: "あいさつ", meaning: "인사", sentence: "挨拶は大事だ。 ", translation: "  인사는중요하다. " },
+
+];
