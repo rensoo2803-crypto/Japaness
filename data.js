100447ab6277453c6c5132a3faa66a0b5ad0bbf3
rensoo2803-crypto <rensoo2803@gmail.com>
2026-05-09 23:59:39 +0900

    Add new vocabulary entries to allvoca array

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const allvoca = [
 { dan: "あ ", word: "愛妻家", reading: "あいさいか", meaning: "애처가", sentence: "吉田さんは愛妻家です。", translation: "よしだ씨는애처가입니다. " },
 { dan: "あ ", word: "挨拶", reading: "あいさつ", meaning: "인사", sentence: "挨拶は大事だ。 ", translation: "  인사는중요하다. " },
-
+{ dan: "か ", word: "換気扇", reading: "かんきせん", meaning: "환풍기", sentence:  "換気かんき扇せんの油汚あぶらよごれの落おとし方 ", translation: "환기팬 기름때 제거법 " },
+{ dan: "す ", word: "擦れる·摩れる·磨れる", reading: "すれる", meaning: "1.	스치다 2.	서로 스쳐서 닳거나 끊어지다 3.	(사람의)가스러지다 닳고닳다", sentence: "1. 木の葉が擦れる 2. 袖口が擦れる 3. 擦れた人 ", translation: " 1.나뭇잎이 스치는 소리 2.소맷부리가 닳다 3.닳고 닳은 사람  " },
 ];
